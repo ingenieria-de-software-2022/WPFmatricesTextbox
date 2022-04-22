5ce5d56ffcc2cb257983ed5cd3cc30bb146a880b
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-04-22 12:21:07 -0500

    feat: adding WPF version of practice #16

---
+++ WPFmatricesTextbox/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFmatricesTextbox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int m, n;
+        Matrix matrixA;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            textBoxMatrixA.Clear();
+            listBoxMatA.Items.Clear();
+        }
+
+        private void buttonRead_Click(object sender, RoutedEventArgs e)
+        {
+            string[] split;
+
+            try
+            {
+                for (int i = 0; i < m; i++)
+                {
+                    split = textBoxMatrixA.GetLineText(i).Split(',');
+
+                    for (int j = 0; j < n; j++)
+                    {
+                        matrixA.Elem[i, j] = double.Parse(split[j]);
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error en la entrada de datos.\nIngrese solo números");
+            }
+        }
+
+        private void buttonPrint_Click(object sender, RoutedEventArgs e)
+        {
+            string aux;
+
+            for (int i = 0; i < m; i++)
+            {
+                aux = " ";
+                for (int j = 0; j < n; j++)
+                {
+                    aux += matrixA.Elem[i, j] + " \t";
+                }
+
+                listBoxMatA.Items.Add(aux);
+            }
+        }
+
+        private void buttonSize_Click(object sender, RoutedEventArgs e)
+        {
+            m = int.Parse(textBoxM.Text);
+            n = int.Parse(textBoxN.Text);
+            matrixA = new Matrix(m, n);
+        }
+    }
+}
---
+++ WPFmatricesTextbox/Matrix.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFmatricesTextbox
+{
+    internal class Matrix
+    {
+        private int m, n;
+        double[,] elem;
+
+        public Matrix(int m,int n)
+        {
+            this.m = m;
+            this.n = n;
+            elem = new double[m, n];
+        }
+
+        public int M
+        {
+            get { return m; }
+            set { m = value; }
+        }
+
+        public int N
+        {
+            get { return n; }
+            set { n = value; }
+        }
+
+        public double[,] Elem
+        {
+            get { return elem; }
+            set { elem = value; }
+        }
+    }
+}
